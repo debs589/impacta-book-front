b453a5cfbfd54ca4f8fa63ff03f2d3b2cc82ac38
debs589 <debora.carvalho589@gmail.com>
2025-04-06 09:44:29 -0300

    load home page

--- src/handlers/pages_handler.go
+++ src/handlers/pages_handler.go
@@ -1,8 +1,16 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"impacta-book/src/config"
+	"impacta-book/src/cookies"
+	"impacta-book/src/models"
+	"impacta-book/src/request"
+	"impacta-book/src/response"
 	"impacta-book/src/utils"
 	"net/http"
+	"strconv"
 )
 
 func LoadLoginPage(w http.ResponseWriter, r *http.Request) {
@@ -15,5 +23,34 @@ func LoadUserRegisterPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func LoadMainPage(w http.ResponseWriter, r *http.Request) {
-	utils.RenderTemplate(w, "home.html", nil)
+	url := fmt.Sprintf("%s/publication", config.APIURL)
+	responseApi, err := request.RequestWithAuthentication(r, http.MethodGet, url, nil)
+	if err != nil {
+		response.JSON(w, http.StatusInternalServerError, response.ErrorAPI{Error: err.Error()})
+		return
+	}
+	defer responseApi.Body.Close()
+
+	if responseApi.StatusCode >= 400 {
+		response.TreatStatusCodeError(w, responseApi)
+		return
+	}
+
+	var publications []models.Publication
+	if err = json.NewDecoder(responseApi.Body).Decode(&publications); err != nil {
+		response.JSON(w, http.StatusUnprocessableEntity, response.ErrorAPI{Error: err.Error()})
+		return
+	}
+
+	cookie, _ := cookies.Read(r)
+	userID, _ := strconv.Atoi(cookie["id"])
+
+	utils.RenderTemplate(w, "home.html", struct {
+		Publications []models.Publication
+		UserID       int
+	}{
+		Publications: publications,
+		UserID:       userID,
+	})
+
 }
